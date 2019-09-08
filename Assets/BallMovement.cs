ada5153a667313fe8f9705bd67a1952a85d1c7b6
haid0109 <51113567+haid0109@users.noreply.github.com>
2019-09-08 13:05:20 +0200

    Uploaded all files

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private Rigidbody2D RB2D; //creates an instance of the "Rigidbody2D" class
+
+    void Start() //"Start()" is called only once at the very beginning of the game
+    {
+        RB2D = GetComponent<Rigidbody2D>(); //makes sure the ball gets the "Rigidbody2D" component at the start of the game
+    }
+
+    void Update() //"Update()" is called once every frame
+    {
+        Vector2 ballPosition = new Vector2(0, RB2D.velocity.y); //creates a vector that still allows the gravity physics of "Rigidbody2D" to work
+
+        if (Input.GetKey(KeyCode.RightArrow)) //executes the code below if the "RighArrow"(->) key is pressed
+        {
+            ballPosition.x = 7; //adds 7 to the balls "x" vector value, which in turn moves it to the right
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ballPosition.x = -7;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //"GetKeyDown" is different from "GetKey" in that you can't hold the key down
+        {                                           //and still get the output, you have to lift your finger from the key before it registers again
+            ballPosition.y = 7; //adds 350 to the balls "y" vector force
+        }
+
+        RB2D.velocity = ballPosition; //moves the ball
+    }
+}
---
+++ Assets/OnCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //allows us to use the "Scenemanager" class
+
+public class OnCollision : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //is executed when an object collides with this object
+    {
+        Destroy(collision.gameObject); //destroys the object that collided this object
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //reloads the current scene
+    }
+}
